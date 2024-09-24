4b55449fcbb911d675b001969c373573faaf8a86
ephanta <eva-maria.boes@informatik.hs-fulda.de>
2024-09-24 17:56:06 +0200

    adding dart doc documentation

--- lib/routes/route_guard.dart
+++ lib/routes/route_guard.dart
@@ -5,14 +5,19 @@ import 'app_router.gr.dart';
 
 /// {@category Routes}
 /// Schützt die Routen der App vor unautorisiertem Zugriff.
+/// Wenn der Nutzer eingeloggt ist, wird die Navigation fortgesetzt.
+/// Andernfalls wird der Nutzer auf die Anmeldeseite umgeleitet.
+/// @nodoc
 class AuthGuard extends AutoRouteGuard {
   @override
   Future<void> onNavigation(
       NavigationResolver resolver, StackRouter router) async {
     final user = Supabase.instance.client.auth.currentUser;
     if (user != null) {
+      // Setzt die Navigation fort
       resolver.next(true);
     } else {
+      // Leitet zur Anmeldeseite um
       router.push(const AuthRoute());
     }
   }
